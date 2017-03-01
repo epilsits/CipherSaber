d4708caf209dd2cbba61241129dc2447a8385870
Erik Pilsits <epilsits@gmail.com>
2017-03-01 13:03:48 -0600

    Add project files.

---
+++ CipherSaber/CipherSaber.cs
@@ -0,0 +1,56 @@
+﻿using Arcfour;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CipherSaber
+{
+    class CipherSaber
+    {
+        static void Main(string[] args)
+        {
+            //Stopwatch stopWatch = new Stopwatch();
+            //Console.WriteLine("Warming up...");
+            //Thread.Sleep(2000);
+            //Console.WriteLine("Go.\n");
+
+            //byte b = 0;
+            //int size = 100 * 1024 * 1024;
+            //for (int k = 0; k < 5; k++)
+            //{
+            //    RC4 rc4 = new RC4("boomstick");
+            //    stopWatch.Restart();
+            //    for (int i = 0; i < size; i++)
+            //        b = rc4.GetNextByte();
+
+            //    stopWatch.Stop();
+            //    Console.WriteLine(string.Format("{2} MB Stream time: {0:n3} sec\nLast byte: 0x{1:x}\n", stopWatch.Elapsed.TotalSeconds, b, size / 1024 / 1024));
+            //}
+
+            //Console.ReadKey();
+
+            RC4 rc4 = new RC4("boomstick");
+            string orig = "Hello World";
+            string enc = rc4.CryptString(orig);
+            rc4 = new RC4("boomstick");
+            string dec = rc4.CryptString(enc);
+            Console.WriteLine(string.Format("Original: {0}\nEncrypted: {1}\nDecrypted: {2}", orig, enc, dec));
+
+            Console.ReadKey();
+
+            //string orig = @"C:\Users\epilsits\Desktop\CSU2.1_For_RDI_Feb2017.msi.zip";
+            //string enc = @"C:\Users\epilsits\Desktop\enc.dat";
+            //string dec = @"C:\Users\epilsits\Desktop\orig.zip";
+
+            //RC4 rc4 = new RC4("boomstick");
+            //rc4.CryptFile(orig, enc);
+
+            //rc4 = new RC4("boomstick");
+            //rc4.CryptFile(enc, dec);
+        }
+    }
+}
---
+++ CipherSaber/RC4.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arcfour
+{
+    class RC4
+    {
+        private byte[] Key { get; set; }
+        private IEnumerator<byte> Gen { get; }
+
+        public RC4(byte[] key, int rounds = 1)
+        {
+            if (key.Length > 256)
+                throw new ArgumentException("Max key length exceeded.");
+
+            InitKey(key, rounds);
+            Gen = GenBytes().GetEnumerator();
+        }
+
+        public RC4(string keytext, int rounds = 1, string encoding = "iso-8859-1")
+            : this(Encoding.GetEncoding(encoding).GetBytes(keytext), rounds) // also Latin1; 8-bit character set
+        {           
+        }
+
+        private void InitKey(byte[] key, int rounds)
+        {
+            int kl = key.Length;
+            Key = Enumerable.Range(0, 256).Select(i => Convert.ToByte(i)).ToArray();
+            int j = 0;
+            for (int r = 0; r < rounds; r++)
+                for (int i = 0; i < 256; i++)
+                {
+                    j = (j + Key[i] + key[i % kl]) % 256;
+                    Swap(ref Key[i], ref Key[j]);
+                }
+        }
+
+        private IEnumerable<byte> GenBytes()
+        {
+            int i = 0;
+            int j = 0;
+            while (true)
+            {
+                i = ++i % 256;
+                j = (j + Key[i]) % 256;
+                Swap(ref Key[i], ref Key[j]);
+                yield return Key[(Key[i] + Key[j]) % 256];
+            }
+        }
+
+        public byte GetNextByte()
+        {
+            Gen.MoveNext();
+            return Gen.Current;
+        }
+
+        public byte[] CryptBytes(byte[] bytes, int len)
+        {
+            for (int i = 0; i < len; i++)
+                bytes[i] = (byte)(bytes[i] ^ GetNextByte());
+
+            return bytes;
+        }
+
+        public byte[] CryptBytes(byte[] bytes)
+        {
+            return CryptBytes(bytes, bytes.Length);
+        }
+
+        public string CryptString(string str, string encoding = "iso-8859-1")
+        {
+            var enc = Encoding.GetEncoding(encoding);
+            return enc.GetString(CryptBytes(enc.GetBytes(str)));
+        }
+
+        public void CryptFile(string inFile, string outFile)
+        {
+            if (!File.Exists(inFile))
+                throw new ArgumentException("Input file does not exist.");
+
+            byte[] buff = new byte[1048576];
+            using (var input = File.OpenRead(inFile))
+            using (var output = File.OpenWrite(outFile))
+            {
+                int read;
+                while ((read = input.Read(buff, 0, 1048576)) > 0)
+                    output.Write(CryptBytes(buff, read), 0, read);
+            }
+        }
+
+        #region Utility
+        public static void Swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        #endregion
+    }
+}
